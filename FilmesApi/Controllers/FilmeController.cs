6c7390d59838a48bde5e7cc00b1f2314af301bd9
J3eff <jeffersonvideo125@gmail.com>
2023-09-13 20:30:52 -0300

    Paginando resultados

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -11,9 +11,9 @@ public class FilmeController : ControllerBase
     private static int id = 1;
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperarFilmes()
+    public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return filmes;
+        return filmes.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
