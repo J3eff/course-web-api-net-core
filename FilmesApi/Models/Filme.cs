a338da2faf66adf806c12ebc0bffdba2252b0ce2
J3eff <jeffersonvideo125@gmail.com>
2023-09-13 20:17:58 -0300

    Recuperando filmes por ID

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -4,11 +4,15 @@ namespace FilmesApi.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
+
     [Required(ErrorMessage = "O titulo do filme é obrigatório")]
     public string Titulo { get; set; }
+
     [Required(ErrorMessage = "O gênero do filme é obrigatório")]
     [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode execeder 50 caracteres")]
     public string Genero { get; set; }
+
     [Required]
     [Range(70,600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
     public int Duracao { get; set; }
