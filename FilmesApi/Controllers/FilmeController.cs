5a3212b6fad86e4950b4c9e67f8263a25223f567
J3eff <jeffersonvideo125@gmail.com>
2023-09-15 01:04:16 -0300

    Atualizando dados com PUT

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -43,4 +43,15 @@ public class FilmeController : ControllerBase
         _context.SaveChanges();
         return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
     }
+
+    [HttpPut("{id}")]
+    public IActionResult AtualizaFilme(int id, [FromBody]UpdateFilmeDto filmeDto)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if(filme == null) return NotFound();
+        _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
 }
