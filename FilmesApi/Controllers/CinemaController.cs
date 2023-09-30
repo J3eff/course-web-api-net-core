ce56bae9ce6ef1834ee3f47508ac57b1ea5c5126
J3eff <jeffersonvideo125@gmail.com>
2023-09-30 17:26:09 -0300

    Adicionando o Cinema

---
+++ FilmesApi/Controllers/CinemaController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    public class CinemaController : ControllerBase
+    {
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+        public CinemaController(FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+                
+
+        [HttpGet]
+        public IEnumerable<ReadCinemaDto> RecuperaCinemas()
+        {
+            return _mapper.Map<List<ReadCinemaDto>>(_context.Cinemas.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaCinemasPorId(int id)
+        {
+            Cinema cinema = _context.Cinemas.FirstOrDefault(cinema => cinema.Id == id);
+            if(cinema != null)
+            {
+                ReadCinemaDto cinemaDto = _mapper.Map<ReadCinemaDto>(cinema);
+                return Ok(cinemaDto);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarCinema([FromBody] CreateCinemaDto cinemaDto)
+        {
+            Cinema cinema = _mapper.Map<Cinema>(cinemaDto);
+            _context.Cinemas.Add(cinema);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaCinemasPorId), new { id = cinema.Id });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaCinema(int id, [FromBody] UpdateCinemaDto cinemaDto)
+        {
+            Cinema cinema = _context.Cinemas.FirstOrDefault(cinema => cinema.Id == id);
+            if(cinema == null)
+                return NotFound();
+            _mapper.Map(cinemaDto, cinema);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaCinema(int id)
+        {
+            Cinema cinema = _context.Cinemas.FirstOrDefault(cinema => cinema.Id == id);
+            if (cinema == null)
+                return NotFound();
+            _context.Remove(cinema);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+    }
+}
--- FilmesApi/Data/FilmeContext.cs
+++ FilmesApi/Data/FilmeContext.cs
@@ -8,6 +8,5 @@ public class FilmeContext : DbContext
     public FilmeContext(DbContextOptions<FilmeContext> opts) : base(opts) { }
 
     public DbSet<Filme> Filmes { get; set; }
-
-
+    public DbSet<Cinema> Cinemas { get; set; }
 }
